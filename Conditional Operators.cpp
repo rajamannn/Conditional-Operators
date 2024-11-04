647f23af1905c3c4793fbc7f0bd78f39ac6cad4a
Aman Raj <160530800+rajamannn@users.noreply.github.com>
2024-11-05 00:48:40 +0530

    Add files via upload

---
+++ Conditional Operators.cpp
@@ -0,0 +1,12 @@
+//Program to print the Conditional Operators in C++
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ int a = 3, b = 4;
+ int result = (a < b) ? b : a;
+    cout << "The greatest number is " << result << endl;
+
+ return 0;s
+}
